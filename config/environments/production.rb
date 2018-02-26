404293fd3f35c60c9080cc9e0ebcffb3c55b03ea
yujunk <yujunk@gmail.com>
2018-02-26 18:02:36 +0800

    included action mailer configs in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'protected-coast-81490.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['MAILER_EMAIL'],
+    password:             ENV['MAILER_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
