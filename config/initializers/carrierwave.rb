c09bdefd8cca925d2f33bfba491f537bbce8e8f4
yujunk <yujunk@gmail.com>
2018-02-26 17:11:47 +0800

    remove comments in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,27 +1,4 @@
-# https://gist.github.com/marcusmalmberg/4073144
 
-# CarrierWave.configure do |config|
-  
-#   # Use local storage if in development or test
-#   if Rails.env.development? || Rails.env.test?
-#     CarrierWave.configure do |config|
-#       config.storage = :fog
-#     end
-#   end
-  
-#   config.fog_credentials = {
-#     :provider               => 'AWS',                             # required
-#     :aws_access_key_id      => 'xx',            # required
-#     :aws_secret_access_key  => 'xx',     # required
-#     :region                 => 'ap-southeast-1'                        # optional, defaults to 'us-east-1'
-#   }
-#   config.fog_directory  = 'next-academy-practice'               # required
-#   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-#   #config.fog_public     = false                                  # optional, defaults to true
-#   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-# end
-
-#https://github.com/carrierwaveuploader/carrierwave#using-amazon-s3
 
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
