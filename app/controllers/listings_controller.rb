668d58f9ce980bd763335aa3b4bfcb5867bfcb23
yujunk <yujunk@gmail.com>
2018-03-13 19:04:01 +0800

    making sure up to date

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -14,13 +14,14 @@ class ListingsController < ApplicationController
 
 	def index	
     parameters = filtering_params(params)
+    #params = params = { listing: { title: }}
     @listings = Listing.find_listings(parameters)
     
     @listings = @listings.order(id: :desc).page params[:page]
 	end
 
 	def edit
-		allowed?(action: @listing, user: current_user)
+		# allowed?(action: @listing, user: current_user)
 		@listing = Listing.find(params[:id])
 	end
 
@@ -39,15 +40,18 @@ class ListingsController < ApplicationController
 private
 
 	def listing_params
-		params.require(:listing).permit(:title, :description, :price_per_night, :smoking, :location, :user_id, :image, amenities: [])
+		byebug
+    params.require(:listing).permit(:title, :description, :price_per_night, :smoking, :location, :user_id, :image, amenities: [])
 	
 	end
 
 	def update_listing_params
-		params.require(:listing).permit(:title, :description, :price_per_night, :smoking, :location)
+		byebug
+    params.require(:listing).permit(:title, :description, :price_per_night, :smoking, :location)
 	end
 
 	def filtering_params(params)
+    byebug
     params.slice(:location, :smoking, :price_per_night)
   end
 
